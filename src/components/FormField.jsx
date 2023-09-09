08a19b1bb3fb592209d654c90059485a391c74ea
Oling Emmanuel Benjamin <olingemmanuel5@gmail.com>
2023-09-09 23:34:56 +0300

    first commit

---
+++ src/components/FormField.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label></label>
+      </div>
+    </div>
+  );
+};
+
+export default FormField;
